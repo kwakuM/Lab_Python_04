bb51c098a74b61d50a6768ec797410bacd7f9ffe
Ananse <ananse@ananse-laptop.(none)>
2012-06-22 17:18:53 +0000

    answers

---
+++ list.py
@@ -0,0 +1,72 @@
+'''a=['a','b','c']
+print a
+print "\n"
+groceries =['bananas','strawberries','apples','bread']
+print groceries
+print "\n"
+print 'we are popping bottles !!!!!'
+print "\n"
+groceries.append('champagne')
+print groceries
+print "\n"
+print 'take bread out !!!'
+print "\n"
+groceries.remove('bread')
+print groceries
+print "\n"
+groceries.sort()
+print groceries'''
+
+'''print data structure should be dictionary
+        since the syocks will be mapped to their
+        respective prices.And also this enables changes
+        to be made to both the key as well as the value'''
+
+
+'''item_price={'apples':7.3,'bananas':5.5,'bread':1.0,'Carrots':10.0,'Champagne':20.90,'Strawberries':32.6}
+print item_price
+
+item_price['Strawberries']=64.43
+print item_price
+
+
+item_price['Chicken']=6.5
+print item_price'''
+
+
+'''print'dictionary will be the best'
+in_stock=item_price
+print "\n"
+#print 'items in stock are',in_stock
+always_in_stock=tuple(in_stock)
+print 'Come to shoprite ! we always sell:',always_in_stock
+for i in always_in_stock:
+    print i  '''
+
+
+print "list inside dictionaries"
+print '\n'
+price=['10','20','30','5']  
+price=[int(e) for e in price]
+price.sort()
+print price
+
+
+new_float=input()
+'''def binary_insert(new_float,price):
+#modifies the input list to include the new_float
+    for i in price:
+    
+        if new_float>i:
+            pass
+            return'''
+
+
+
+
+
+
+
+
+
+return
